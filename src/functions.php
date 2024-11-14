ed5596e510ddcb6e4bf609bc9801e531226a809b
Alex Law <Cleancookie@users.noreply.github.com>
2024-11-14 09:14:30 +0000

    ✏️ Add docblock to have the same return type as the provided callback

--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,11 @@
 <?php
 
 if (!function_exists('attempt')) {
+    /**
+     * @template T
+     * @param callable():T $callback The callback to attempt
+     * @return array{0: \Throwable|null, 1: T|null} A tuple where the first item is an error (if any) and the second item is the result of the callback
+     */
     function attempt($callback)
     {
         try {
